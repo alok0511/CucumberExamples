3c7bf5197a6033070cb68480993ccad667873936
alok0511 <30834567+alok0511@users.noreply.github.com>
2019-03-14 14:26:46 +0530

    Add files via upload

---
+++ EmoloyeeSalaryManagerExample/src/main/java/com/capgemini/salary/Employee.java
@@ -0,0 +1,39 @@
+package com.capgemini.salary;
+
+public class Employee {
+
+	private int id;
+	private String user;
+	private float salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	public Employee(int id, String user, float salary) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
+
+	
